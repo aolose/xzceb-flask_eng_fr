dc46efc1cac2df01afba25384cc601d5f7c035fb
z <aolose@fsm.cc>
2023-07-21 16:56:35 +0800

    lan

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -2,10 +2,10 @@ from deep_translator import MyMemoryTranslator
 
 
 def englishToFrench(english_text):
-    french_text = MyMemoryTranslator(source='en-US', target='fr-FR').translate(english_text)
+    french_text = MyMemoryTranslator(source='en', target='fr').translate(english_text)
     return french_text
 
 
 def frenchToEnglish(french_text):
-    english_text = MyMemoryTranslator(source='fr-FR', target='en-US').translate(french_text)
+    english_text = MyMemoryTranslator(source='fr', target='en').translate(french_text)
     return english_text
